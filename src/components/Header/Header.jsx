33b1821f0c43458b52aa08d894d604ebc57eff28
OstapoKapo <OstapoKapo@gmail.com>
2023-07-17 19:14:59 +0300

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import './Header.css'
+import logo from './img/logo.png'
 
 export default function Header() {
     return (
-        <div>
-            <h1 className='header'>Todo list</h1>
+        <div className='header'>
+            <div className="header__logo">
+                <img src={logo} alt="logo" />
+            </div>
+            <h1 className='header__name'>TodoList</h1>
         </div>
     )
 }
